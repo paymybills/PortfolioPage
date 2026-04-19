67308e3a2cd26e8fc16ce69b27f16f12d00f17b6
Aniruddha Roy <anni.kelp@example.com>
2026-04-19 23:19:41 +0530

    Fix LiquidEther typescript signature errors for production build

--- src/components/LiquidEther.tsx
+++ src/components/LiquidEther.tsx
@@ -528,7 +528,7 @@ export default function LiquidEther({
         this.props = props || {};
         this.uniforms = this.props.material?.uniforms;
       }
-      init() {
+      init(args?: any) {
         this.scene = new THREE.Scene();
         this.camera = new THREE.Camera();
         if (this.uniforms) {
@@ -538,7 +538,7 @@ export default function LiquidEther({
           this.scene.add(this.plane);
         }
       }
-      update() {
+      update(args?: any) {
         Common.renderer.setRenderTarget(this.props.output || null);
         Common.renderer.render(this.scene, this.camera);
         Common.renderer.setRenderTarget(null);
